30e449f8dd1368fec26c98937e67174657ea956f
Sumit <goyalsumit1095@gmail.com>
2024-10-15 14:06:59 +0100

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+function name(){
+return(
+  <hi> i am ankith</hi>
+)
+}
+
+createRoot(document.getElementById('root')).render(
+<>
+<name/></>
+  
+)
